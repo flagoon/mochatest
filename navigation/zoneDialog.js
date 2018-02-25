e7e70d8bd66dd12805f347aabb7c6cada42d5df0
Paweł Kochanek <flagoon@o2.pl>
2018-02-25 21:51:45 +0100

    Some renaming.

--- navigation/zoneDialog.js
+++ navigation/zoneDialog.js
@@ -4,17 +4,17 @@ module.exports = {
     /**
      * Dialog elements.
      */
-    ZDIALOG_ZONE_NAME: By.id('zone_name'),
-    ZDIALOG_DOMAIN_NAME: By.id('domain_name_zone'),
-    ZDIALOG_ADD_DOMAIN: By.id('add_another_domain'),
-    ZDIALOG_REMOVE_DOMAIN: By.id('remove_domain'),
-    ZDIALOG_CANCEL_BUTTON: By.id('cancel_button'),
-    ZDIALOG_SUBMIT: By.id('submit'),
-    ZDIALOG_UPDATE: By.id('update'),
+    ZONE_NAME: By.id('zone_name'),
+    DOMAIN_NAME: By.id('domain_name_zone'),
+    ADD_DOMAIN: By.id('add_another_domain'),
+    REMOVE_DOMAIN: By.id('remove_domain'),
+    CANCEL_BUTTON: By.id('cancel_button'),
+    SUBMIT: By.id('submit'),
+    UPDATE: By.id('update'),
     /**
      * Data for forms.
      */
-    ZDIALOG_ZONE_CHANGE: 'szczecin',
-    ZDIALOG_ZONE_CHANGED: 'koszalin',
-    ZDIALOG_DOMAIN_CHANGE: 'domain.to.change'
+    ZONE_CHANGE: 'szczecin',
+    ZONE_CHANGED: 'koszalin',
+    DOMAIN_CHANGE: 'domain.to.change'
 };
