60f261e172d53a4c8e137cfcb3d25e2e626c88a3
Paweł Kochanek <flagoon@o2.pl>
2018-02-25 20:45:28 +0100

    Removed chai.

--- config/driver.js
+++ config/driver.js
@@ -1,22 +1,14 @@
 const { Builder, By, until, Key } = require('selenium-webdriver');
 const webdriver = require('selenium-webdriver');
-const chai = require('chai');
 
 exports.buildDriver = () => {
     const chromeCapabilities = webdriver.Capabilities.chrome();
     const chromeOptions = {
-        args: [
-            '--test-type',
-            '--start-maximized',
-            '--disable-web-security',
-            '--ignore-certificate-errors'
-        ]
+        args: ['--test-type', '--start-maximized', '--disable-web-security', '--ignore-certificate-errors']
     };
     chromeCapabilities.set('chromeOptions', chromeOptions);
     return new Builder().withCapabilities(chromeCapabilities).build();
 };
-
-exports.chai = chai;
 exports.By = By;
 exports.until = until;
 exports.Key = Key;
