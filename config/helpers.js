052ac2e020b80e910cad6d936b0309ab78cae16c
Paweł Kochanek <flagoon@o2.pl>
2018-02-25 21:51:29 +0100

    Created new helpers functions.

--- config/helpers.js
+++ config/helpers.js
@@ -1,13 +1,30 @@
 const { until } = require('selenium-webdriver');
+const Constants = require('../config/Constants');
 
-const chalk = require(chalk);
+const chalk = require('chalk');
 
-exports.waitForElement = function(locator, driver, timeout = Constants.DEFAULT_TIMEOUT) {
+const chalkError = chalk.bgRed.white.bold;
+const chalkInfo = chalk.bgWhite.blue;
+
+const waitForElement = function(locator, driver, timeout = Constants.DEFAULT_TIMEOUT) {
     try {
         return driver.wait(until.elementLocated(locator), timeout);
     } catch (error) {
         console.log(chalkError(`No such locotar - ${locator}`));
     }
+};
+
+exports.waitForElement = waitForElement;
+
+exports.clickWebElement = async function(locator, driver, success, error) {
+    await waitForElement(locator, driver)
+        .then(async element => {
+            await element.click();
+            console.log(chalkInfo(success));
+        })
+        .catch(() => {
+            throw new Error(chalkError(error));
+        });
 };
 
 exports.chalkError = chalk.bgRed.white.bold;
